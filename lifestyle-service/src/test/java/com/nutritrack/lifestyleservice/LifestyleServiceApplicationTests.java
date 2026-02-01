51ab2060e776c6064af946cadfb51c298cc62ecc
AShaik <absar.unix@gmail.com>
2026-02-01 00:10:50 +0000

    Initialize Spring Boot microservice modules with Spring Initializr

---
+++ lifestyle-service/src/test/java/com/nutritrack/lifestyleservice/LifestyleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutritrack.lifestyleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifestyleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ nutrition-service/src/test/java/com/nutritrack/nutritionservice/NutritionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutritrack.nutritionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
